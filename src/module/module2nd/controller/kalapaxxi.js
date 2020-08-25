6e882207f1f24d304e9f5165f22088119cce3d0d
vberzsin <vberzsin@web.de>
2020-08-25 06:40:54 +0200

    docidoci

--- src/module/module2nd/controller/kalapaxxi.js
+++ src/module/module2nd/controller/kalapaxxi.js
@@ -8,6 +8,7 @@ let Kalapaxxi = function( messageQueue, model, link, view ) {
 	this.init = function () {
 
 		this.messageQueue.register ( new Event( 'Model.UPDATED' ), this, 'update' );
+		this.messageQueue.register ( new Event( 'Lunk.SET' ), this, 'logTheDeeplink' );
 		this.messageQueue.notify ( new Event( 'Kalapaxxi.INITED', { date: new Date() } ) );	
 	}
 
@@ -16,4 +17,8 @@ let Kalapaxxi = function( messageQueue, model, link, view ) {
 		document.write( this.view.replace( '{var1st}', '491' ) ); 
 		console.log( '>>> update(): ', event );
 	}
+
+	this.logTheDeeplink = function ( event ) {
+		console.log( '>>> logTheDeeplink(): ', event.model );
+	} 
 }
