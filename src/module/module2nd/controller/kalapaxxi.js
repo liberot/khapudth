41f6c2f1729b446ae2ce36a0fd1dac7d61475cd3
vberzsin <vberzsin@web.de>
2020-08-25 19:56:38 +0200

    templ

--- src/module/module2nd/controller/kalapaxxi.js
+++ src/module/module2nd/controller/kalapaxxi.js
@@ -18,13 +18,15 @@ let Kalapaxxi = function( messageQueue, model, link, view ) {
 		this.model.var2nd = 'The Continious Spirit of Zwagazhul Allmighty';
 
 		// template variable vs. model variable fertauschi
+		// ..{variable}..... -> this.model.variable
 		let vars = this.view.match(/\{(.{1,32}?)\}/g);
 		for( var idx in vars ){
 			let index = vars[ idx ];
 			let title = vars[ idx ].replace( /[\{\}]/g, '' );
 			let value = this.model[ title ];
-			this.view = this.view.replace( index, value );
-			// console.log( 'title: ', title, 'value: ', value ); 
+			if( null != value ) {
+				this.view = this.view.replace( index, value );
+			}
 		}
 
 		document.write( this.view ); 
